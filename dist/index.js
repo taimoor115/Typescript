5f1e3e5eca4247f9e4017a6f3ab278b7febbe53d
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-13 13:22:22 +0500

    Machine Setup

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello");
+let age = 20;
+if (age > 15)
+    age += 5;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Hello");
+// "preLaunchTask": "tsc: build - tsconfig.json",
+let age: number = 20
+if(age > 15)
+  age += 5;
