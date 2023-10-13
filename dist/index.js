e2ce813aa2802838af2996f345dcf85cee4c0abe
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-13 14:57:23 +0500

    Arrays, Tuples, Enum

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,26 @@
 "use strict";
-console.log("Hello");
-let age = 20;
-if (age > 15)
-    age += 5;
+let sales = 123456789;
+let course = "TypeScript";
+let isPublished = true;
+let level;
+level = 12;
+level = "Any mean any type but we loose typescript major feature";
+function render(document) {
+    console.log('Hello');
+}
+const arr = [1, 2, 3, 4];
+let tuple = [12, 'Taimoor', true];
+tuple.push(1);
+tuple[1].charAt(0);
+let graph = [1.2, 4.1];
+let [x, y] = graph;
+const vehicles = ['Bolan', 'BMW', 'Ford'];
+const [van, car, truck] = vehicles;
+console.log(van);
+const small = 1;
+const medium = 2;
+const large = 3;
+;
+let mySize = "m";
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,61 @@
-console.log("Hello");
-// "preLaunchTask": "tsc: build - tsconfig.json",
-let age: number = 20
-if(age > 15)
-  age += 5;
+// console.log("Hello");
+// // "preLaunchTask": "tsc: build - tsconfig.json",
+// let age: number = 20
+// if(age > 15)
+//   age += 5;
+
+let sales = 123_456_789
+let course = "TypeScript"
+let isPublished = true;
+let level; //any
+
+level = 12;
+level = "Any mean any type but we loose typescript major feature"
+
+function render(document: any) { // as we see error but we can fix using any but we loose typescript property
+  console.log('Hello');
+
+}
+
+// Arrays
+
+const arr: number[] = [1,2,3,4];
+
+// arr.forEach(n => n.toString) // we get all built function of number
+
+// Tuples
+// A tuple is a typed array with a pre-defined length and types for each index.
+
+let tuple: [number, string, boolean] = [12, 'Taimoor', true];
+tuple.push(1); // as you see it shows no error this is the gap in typescript
+tuple[1].charAt(0); // each index will enable each type built in method
+
+// Always try to make readonly tuple this will give you error if you push method
+// const ourReadonlyTuple: readonly [number, boolean, string] = [5, true, 'The Real Coding God'];
+// throws error as it is readonly.
+// ourReadonlyTuple.push('Coding God took a day off');
+
+// Destructure
+let graph: [number, number] = [1.2, 4.1];
+let [x,y] = graph;
+
+const vehicles: [string, string, string] = ['Bolan', 'BMW', 'Ford'];
+const [van, car, truck] = vehicles;
+console.log(van);
+
+
+// ENUM
+// An enum is a special "class" that represents a group of constants (unchangeable variables).
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// enum Size { Small = 1, Medium, Large};
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+// We can change the value to
+
+const enum Size { Small = 's', Medium = 'm', Large = 'l'};
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
